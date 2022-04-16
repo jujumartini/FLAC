e398bcd0c54df33d33252850adf777d5699c3f0e
Julian END416 <marti994@uwm.edu>
2022-04-16 15:53:43 -0500

    style(acc_models): umass.wrist
    - Add space at start/end of function.
    - Make assignments to objects start on a newline.
    - Add argument names to functions used.

--- R/acc_models/acc_models.R
+++ R/acc_models/acc_models.R
@@ -200,62 +200,136 @@ hildebrand <- function(ag_data_raw_wrist,
   
 }    
 
-umass.wrist <- function(ag_data_raw_wrist,freq=100)
-{
-  win.width <- 15
+umass.wrist <- function(ag_data_raw_wrist,
+                        freq = 100) {
   
-  n <- dim(ag_data_raw_wrist)[1]
+  win.width <- 
+    15
+  n <- 
+    dim(ag_data_raw_wrist)[1]
   
-  mins <- ceiling(n/(freq*win.width))  # this is really the number of 15-sec windows in the file 
+  mins <- 
+    ceiling(n / (freq * win.width))  # this is really the number of 15-sec windows in the file 
   
-  ag_data_raw_wrist$min <- rep(1:mins,each=win.width*100)[1:n]
-  ag_data_raw_wrist$v.ang <- 90*(asin(ag_data_raw_wrist$AxisX/ag_data_raw_wrist$VM)/(pi/2))
-  ag_data_raw_wrist_Staud <- data.frame(mean.vm=tapply(ag_data_raw_wrist$VM,ag_data_raw_wrist$min,mean,na.rm=T),
-                                        sd.vm=tapply(ag_data_raw_wrist$VM,ag_data_raw_wrist$min,sd,na.rm=T),
-                                        mean.ang=tapply(ag_data_raw_wrist$v.ang,ag_data_raw_wrist$min,mean,na.rm=T),
-                                        sd.ang=tapply(ag_data_raw_wrist$v.ang,ag_data_raw_wrist$min,sd,na.rm=T),
-                                        p625=tapply(ag_data_raw_wrist$VM,ag_data_raw_wrist$min,pow.625),
-                                        dfreq=tapply(ag_data_raw_wrist$VM,ag_data_raw_wrist$min,dom.freq),
-                                        ratio.df=tapply(ag_data_raw_wrist$VM,ag_data_raw_wrist$min,frac.pow.dom.freq))
+  ag_data_raw_wrist$min <- 
+    rep(1:mins,
+        each = win.width * 100)[1:n]
+  ag_data_raw_wrist$v.ang <- 
+    90 * (asin(ag_data_raw_wrist$AxisX / ag_data_raw_wrist$VM) / (pi / 2))
+  ag_data_raw_wrist_Staud <- 
+    data.frame(
+      mean.vm = 
+        tapply(ag_data_raw_wrist$VM,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = mean,
+               na.rm = TRUE),
+      sd.vm = 
+        tapply(ag_data_raw_wrist$VM,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = sd,
+               na.rm = TRUE),
+      mean.ang = 
+        tapply(ag_data_raw_wrist$v.ang,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = mean,
+               na.rm = TRUE),
+      sd.ang = 
+        tapply(ag_data_raw_wrist$v.ang,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = sd,
+               na.rm = TRUE),
+      p625 = 
+        tapply(ag_data_raw_wrist$VM,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = pow.625),
+      dfreq = 
+        tapply(ag_data_raw_wrist$VM,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = dom.freq),
+      ratio.df = 
+        tapply(ag_data_raw_wrist$VM,
+               INDEX = ag_data_raw_wrist$min,
+               FUN   = frac.pow.dom.freq)
+    )
   
   # apply the models (estimates are for each 15 second epoch)
   
   # MET estimates by random forest
-  ag_data_raw_wrist_Staud$METs.rf <- predict(rf.met.model,newdata=ag_data_raw_wrist_Staud)
-  ag_data_raw_wrist_Staud$METs.rf[ag_data_raw_wrist_Staud$sd.vm<.01] <- 1
+  ag_data_raw_wrist_Staud$METs.rf <- 
+    predict(rf.met.model,
+            newdata = ag_data_raw_wrist_Staud)
+  ag_data_raw_wrist_Staud$METs.rf[ag_data_raw_wrist_Staud$sd.vm < .01] <- 
+    1
   
   # MET estimates by linear regression
-  ag_data_raw_wrist_Staud$METs.lm <- predict(lm.met.model,newdata=ag_data_raw_wrist_Staud)
-  ag_data_raw_wrist_Staud$METs.lm[ag_data_raw_wrist_Staud$sd.vm<.01] <- 1
+  ag_data_raw_wrist_Staud$METs.lm <- 
+    predict(lm.met.model,
+            newdata = ag_data_raw_wrist_Staud)
+  ag_data_raw_wrist_Staud$METs.lm[ag_data_raw_wrist_Staud$sd.vm < .01] <- 
+    1
   
   # MET level estimates (rf and tree)
-  ag_data_raw_wrist_Staud$MET.lev.rf <- predict(rf.met.level.model,newdata=ag_data_raw_wrist_Staud)
-  ag_data_raw_wrist_Staud$MET.lev.tr <- predict(tr.met.level.model,newdata=ag_data_raw_wrist_Staud,type="class")
+  ag_data_raw_wrist_Staud$MET.lev.rf <- 
+    predict(rf.met.level.model,
+            newdata = ag_data_raw_wrist_Staud)
+  ag_data_raw_wrist_Staud$MET.lev.tr <- 
+    predict(tr.met.level.model,
+            newdata = ag_data_raw_wrist_Staud,
+            type = "class")
   
   # sedentary or not estimates (rf and tree)
-  ag_data_raw_wrist_Staud$sed.rf <- predict(rf.sed.model,newdata=ag_data_raw_wrist_Staud)
-  ag_data_raw_wrist_Staud$sed.tr <- predict(tr.sed.model,newdata=ag_data_raw_wrist_Staud,type="class")
+  ag_data_raw_wrist_Staud$sed.rf <- 
+    predict(rf.sed.model,
+            newdata = ag_data_raw_wrist_Staud)
+  ag_data_raw_wrist_Staud$sed.tr <- 
+    predict(tr.sed.model,
+            newdata = ag_data_raw_wrist_Staud,
+            type = "class")
   
   # locomotion or not estimates (rf and tree)
-  ag_data_raw_wrist_Staud$loc.rf <- predict(rf.loc.model,newdata=ag_data_raw_wrist_Staud)
-  ag_data_raw_wrist_Staud$loc.tr <- predict(tr.loc.model,newdata=ag_data_raw_wrist_Staud,type="class")
+  ag_data_raw_wrist_Staud$loc.rf <- 
+    predict(rf.loc.model,
+            newdata = ag_data_raw_wrist_Staud)
+  ag_data_raw_wrist_Staud$loc.tr <- 
+    predict(tr.loc.model,
+            newdata = ag_data_raw_wrist_Staud,
+            type = "class")
   
   # append model estimates back to main data frame
+  METs.rf <- 
+    rep(ag_data_raw_wrist_Staud$METs.rf,
+        each = win.width)
+  METs.lm <- 
+    rep(ag_data_raw_wrist_Staud$METs.lm,
+        each = win.width)
+  MET.lev.tr <- 
+    rep(ag_data_raw_wrist_Staud$MET.lev.tr,
+        each = win.width)
+  MET.lev.rf <- 
+    rep(ag_data_raw_wrist_Staud$MET.lev.rf,
+        each = win.width)
+  sed.rf <- 
+    rep(ag_data_raw_wrist_Staud$sed.rf,
+        each = win.width)
+  sed.tr <- 
+    rep(ag_data_raw_wrist_Staud$sed.tr,
+        each = win.width)
+  loc.rf <- 
+    rep(ag_data_raw_wrist_Staud$loc.rf,
+        each = win.width)
+  loc.tr <- 
+    rep(ag_data_raw_wrist_Staud$loc.tr,
+        each = win.width)
   
-  METs.rf = rep(ag_data_raw_wrist_Staud$METs.rf, each = win.width)
-  METs.lm = rep(ag_data_raw_wrist_Staud$METs.lm, each = win.width)
-  MET.lev.tr = rep(ag_data_raw_wrist_Staud$MET.lev.tr, each = win.width)
-  MET.lev.rf = rep(ag_data_raw_wrist_Staud$MET.lev.rf, each = win.width)
-  sed.rf = rep(ag_data_raw_wrist_Staud$sed.rf, each = win.width)
-  sed.tr = rep(ag_data_raw_wrist_Staud$sed.tr, each = win.width)
-  loc.rf = rep(ag_data_raw_wrist_Staud$loc.rf, each = win.width)
-  loc.tr = rep(ag_data_raw_wrist_Staud$loc.tr, each = win.width)
+  junk <- 
+    rep("sed",
+        times = length(ag_data_raw_wrist_Staud$METs.rf))
+  junk[ag_data_raw_wrist_Staud$METs.rf > 1.5] <- 
+    "light"
+  junk[ag_data_raw_wrist_Staud$METs.rf >= 3] <- 
+    "mvpa"
   
-  junk <- rep("sed",length(ag_data_raw_wrist_Staud$METs.rf))
-  junk[ag_data_raw_wrist_Staud$METs.rf>1.5] <- "light"
-  junk[ag_data_raw_wrist_Staud$METs.rf>=3] <- "mvpa"
-  
-  
-  return(rep(junk,each=15)[1:floor(n/100)])
+  return(rep(junk,
+             each = 15)[1:floor(n / 100)])
 }
 
